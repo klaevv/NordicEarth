9f783ae90adab5a66b4a5257c56710418ac17e4c
Leevi Heino <leeviheino@live.fi>
2019-11-15 09:54:39 +0000

    Lint BonusMap

--- front/src/pages/BonusMap.js
+++ front/src/pages/BonusMap.js
@@ -1,27 +1,28 @@
-import React, { Component } from "react";
-import ThreeModule from "../ThreeModuleBonus";
+import React, { Component } from 'react'
+import ThreeModule from '../ThreeModuleBonus'
 
 class MapPage extends Component {
   constructor(props) {
-    super(props);
-    this.mapRef = React.createRef();
+    super(props)
+    this.mapRef = React.createRef()
   }
 
   componentDidMount() {
+    // eslint-disable-next-line no-new
     new ThreeModule({
       ref: this.mapRef.current,
       displacementScale: 200,
-      imageMap: "./data/team1.png",
-      textureMap: "./data/team1_bw.png"
+      imageMap: './data/team1.png',
+      textureMap: './data/team1_bw.png'
       // groundPosX: 0,
       // groundPosY: 0,
       // groundPosZ: 0
-    });
+    })
   }
 
   render() {
-    return <div ref={this.mapRef}> </div>;
+    return <div ref={this.mapRef}> </div>
   }
 }
 
-export default MapPage;
+export default MapPage
